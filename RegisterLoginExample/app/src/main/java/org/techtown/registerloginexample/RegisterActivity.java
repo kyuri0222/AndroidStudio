0a01220b1df01fee4dff6e447b97a789f0ac66fc
Kyuri Kim <kuri222@naver.com>
2021-02-13 23:35:00 +0900

    Update RegisterActivity

--- RegisterLoginExample/app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
+++ RegisterLoginExample/app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
@@ -1,14 +1,49 @@
 package org.techtown.registerloginexample;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.android.volley.Response;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private EditText et_id, et_pass, et_name, et_age;
+    private Button btn_register;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //액티비티 시작시 처음으로 실행되는 생명주기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // 아이디 값 찾아주기
+        et_id = findViewById(R.id.et_id);
+        et_pass = findViewById(R.id.et_pass);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+
+        // 회원가입 버튼 클릭 시 수행
+        btn_register=findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // EditText에 현재 입력되어있는 값을 get해온다(가져온다).
+                String userID = et_id.getText().toString();
+                String userPass = et_pass.getText().toString();
+                String userName = et_name.getText().toString();
+                int userAge = Integer.parseInt(et_age.getText().toString());
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                };
+            }
+        });
+
     }
 }
