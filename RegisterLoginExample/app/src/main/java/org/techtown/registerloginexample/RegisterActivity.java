5d23f1d67acb208ea28ef44deaa4096f885842b2
Kyuri Kim <kuri222@naver.com>
2021-02-14 04:15:40 +0900

    Update RegisterActivity

--- RegisterLoginExample/app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
+++ RegisterLoginExample/app/src/main/java/org/techtown/registerloginexample/RegisterActivity.java
@@ -1,13 +1,20 @@
 package org.techtown.registerloginexample;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -39,9 +46,29 @@ public class RegisterActivity extends AppCompatActivity {
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) { // 회원 등록에 성공한 경우
+                                Toast.makeText(getApplicationContext(), "회원 등록에 성공하였습니다.",Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+                            }else { // 회원 등록에 실패한 경우
+                                Toast.makeText(getApplicationContext(), "회원 등록에 실패하였습니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
                 };
+
+                // 서버로 Volley를 이용해서 요청을 함
+                RegisterRequest registerRequest = new RegisterRequest(userID,userPass,userName,userAge, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
             }
         });
 
