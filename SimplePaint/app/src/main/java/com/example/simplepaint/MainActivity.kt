7115251542f51a9cd2f88df89f695e9ed023230b
Kyuri Kim <kuri222@naver.com>
2022-05-20 18:26:42 +0900

    SimplePaint

--- SimplePaint/app/src/main/java/com/example/simplepaint/MainActivity.kt
+++ SimplePaint/app/src/main/java/com/example/simplepaint/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.simplepaint
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -15,14 +13,17 @@ class MainActivity : AppCompatActivity() {
     companion object{
         const val LINE = 1
         const val CIRCLE = 2
-        const val RECT = 3
-
+        const val RECTANGLE = 3
         var curShape = LINE
+
+        internal var historyList : MutableList<HistoryList> = ArrayList()
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(SimplePainter(this))
     }
+
     override fun onCreateOptionsMenu(menu: Menu?) : Boolean{
         menu?.add(0,1,0,"Draw Line")
         menu?.add(0,2,0,"Draw Circle")
@@ -30,6 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             1-> {
@@ -41,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             3-> {
-                curShape = RECT
+                curShape = RECTANGLE
                 return true
             }
         }
@@ -49,48 +51,98 @@ class MainActivity : AppCompatActivity() {
     }
 
     class SimplePainter(context: Context) : View(context){
-        var startX = -1f
-        var startY = -1f
-        var stopX = -1f
-        var stopY = -1f
+        var startX = -1
+        var startY = -1
+        var stopX = -1
+        var stopY = -1
         val paint = Paint()
+
         override fun onTouchEvent(event: MotionEvent?) : Boolean{
             when (event?.action){
                 MotionEvent.ACTION_DOWN -> {
-                    startX = event.x
-                    startY = event.y
+                    startX = event.x.toInt()
+                    startY = event.y.toInt()
                 }
-                MotionEvent.ACTION_MOVE,MotionEvent.ACTION_UP -> {
-                    stopX = event.x
-                    stopY = event.y
+                MotionEvent.ACTION_MOVE -> {
+                    stopX = event.x.toInt()
+                    stopY = event.y.toInt()
+                    this.invalidate()
+                }
+                MotionEvent.ACTION_UP -> {
+                    val shape = HistoryList()
+                    shape.shapeType = curShape
+                    shape.startX = startX
+                    shape.startY = startY
+                    shape.stopX = stopX
+                    shape.stopY = stopY
+                    historyList.add(shape)
+
                     this.invalidate()
                 }
             }
             return true
         }
-        override fun onDraw(canvas: Canvas?){
+
+        override fun onDraw(canvas: Canvas){
             super.onDraw(canvas)
             paint.isAntiAlias = true
             paint.strokeWidth = 5f
             paint.style = Paint.Style.STROKE
             paint.color = Color.RED
+
+            for (i in historyList.indices) {
+                val shape = historyList[i]
+                when (shape.shapeType){
+                    LINE -> canvas.drawLine(
+                        shape.startX.toFloat(),
+                        shape.startY.toFloat(),
+                        shape.stopX.toFloat(),
+                        shape.stopY.toFloat(),
+                        paint
+                    )
+
+                    CIRCLE -> {
+                        val radius = Math.sqrt(
+                            Math.pow(
+                                (shape.stopX - shape.startX).toDouble(),
+                                2.0
+                            ) +  Math.pow(
+                                (shape.stopY - shape.startY).toDouble(),
+                                2.0
+                            )
+                        ).toInt()
+                        canvas.drawCircle(
+                            shape.startX.toFloat(),
+                            shape.startY.toFloat(),
+                            radius.toFloat(),
+                            paint
+                        )
+                    }
+
+                    RECTANGLE -> canvas.drawRect(
+                        shape.startX.toFloat(),
+                        shape.startY.toFloat(),
+                        shape.stopX.toFloat(),
+                        shape.stopY.toFloat(),
+                        paint
+                    )
+                }
+            }
+
             when (curShape){
                 LINE -> {
-                    canvas?.drawLine(startX,startY,stopX,stopY,paint)
+                    canvas?.drawLine(startX.toFloat(),startY.toFloat(),stopX.toFloat(),stopY.toFloat(),paint)
                 }
                 CIRCLE ->{
                     val radius = Math.sqrt(Math.pow((stopX-startX).toDouble(),2.0)+
                             Math.pow((stopY-startY).toDouble(),2.0))
-                    canvas?.drawCircle(startX,startY,radius.toFloat(),paint)
+                    canvas?.drawCircle(startX.toFloat(),startY.toFloat(),radius.toFloat(),paint)
                 }
-                RECT ->{
-                    val radius = Math.sqrt(Math.pow((stopX-startX).toDouble(),2.0)+
-                            Math.pow((stopY-startY).toDouble(),2.0))
-                    canvas?.drawCircle(startX,startY,radius.toFloat(),paint)
+                RECTANGLE->{
+                    val rect = Rect (startX, startX, stopX, stopY)
+                    canvas?.drawRect(rect, paint)
                 }
             }
         }
     }
-
-
 }
